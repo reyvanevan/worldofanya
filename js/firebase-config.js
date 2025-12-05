6de646e771e92f1d789319280f89dc75e831e0a9
reyvan.evan <vant8forbid@gmail.com>
2025-12-05 15:32:49 +0700

    Add firebase-config.js for production

---
+++ js/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAZgIedVYK38fyomVGOgg5Sr9biCsZkkCg",
+    authDomain: "zalfauniverse-50531.firebaseapp.com",
+    projectId: "zalfauniverse-50531",
+    storageBucket: "zalfauniverse-50531.firebasestorage.app",
+    messagingSenderId: "187990396550",
+    appId: "1:187990396550:web:366c9d53b2b2e55d3d8b07",
+    measurementId: "G-V9HS9DNLZR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { app, auth, analytics };
